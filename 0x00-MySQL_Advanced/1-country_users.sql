4b5752b5632ecdbfc06932162c1ef12cc6747c2c
Jeanluchabi <jeanuchabi@gmail.com>
2024-06-14 03:10:02 +0200

    adding some files 6 to 101

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- SQL script that creates a table users
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Create table users with specified attributes including an enumeration for country
+
+-- CREATE TABLE query to create table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
